76e817319fdb81434a23edb1b482fdf7f2b7704b
BulatDes <bulatdespizek@gmail.com>
2023-03-22 14:44:36 +0500

    Макеты

---
+++ app/src/main/java/com/bignerdranch/android/makets/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.makets
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun dfs(view: View)
+    {
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+}
+
+
+
